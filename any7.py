6a9d18c5697c639e688c2be760e823bc39b7a991
Menashe <mborukhov770@gmail.com>
2023-01-18 15:20:20 -0700

    any7.py

--- any7.py
+++ any7.py
@@ -1,8 +1,17 @@
 def any7(nums):
     """Are any of these numbers a 7? (True/False)"""
 
-    # YOUR CODE HERE 
+    # for num in nums:
+    #     if num == 7:
+    #         return True
+
+    # return False
+
+    if nums.count(7) > 0:
+        return True
+    else:
+        return False
+
 
 print("should be true", any7([1, 2, 7, 4, 5]))
 print("should be false", any7([1, 2, 4, 5]))
-
